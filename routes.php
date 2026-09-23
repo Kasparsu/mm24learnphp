d7fcce5360b755c817044737a49c7f8497aa7ab7
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-23 14:06:07 +0300

    Add ArticlesController and index and create pages for articles

--- routes.php
+++ routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Controllers\ArticlesController;
 use App\Controllers\PublicController;
 use App\Router;
 
@@ -9,3 +10,8 @@ Router::get('/us', [PublicController::class, 'us']);
 
 Router::get('/forms', [PublicController::class, 'forms']);
 Router::post('/forms', [PublicController::class, 'answer']);
+
+
+Router::get('/admin/articles', [ArticlesController::class, 'index']);
+Router::get('/admin/articles/create', [ArticlesController::class, 'create']);
+Router::post('/admin/articles', [ArticlesController::class, 'store']);
