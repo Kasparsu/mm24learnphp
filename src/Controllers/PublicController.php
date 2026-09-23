edde567c8cfd0ebdce73ef6912e56392d6ca09e1
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-09-23 10:01:03 +0300

    Show articles from database

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -12,43 +12,15 @@ class PublicController
     {
         
         $articles = Article::all();
-        dump($articles);
-        $users = User::all();
-        dump($users);
-        // $title = 'World';
-        // view('index', compact('title', 'posts'));
+        $title = 'World';
+        view('index', compact('title', 'articles'));
     }
 
     public function us()
     {
+        $articles = Article::all();
         $title = 'U.S';
-        $posts = [
-            [
-                'title' => 'Some U.S title 1',
-                'content' => 'Some U.S content 1',
-                'date' => 'January 1, 2021',
-                'author' => 'Pets',
-            ],
-            [
-                'title' => 'Some U.S title 2',
-                'content' => 'Some U.S content 2',
-                'date' => 'January 2, 2021',
-                'author' => 'Juss',
-            ],
-            [
-                'title' => 'Some U.S title 3',
-                'content' => 'Some U.S content 3',
-                'date' => 'January 3, 2021',
-                'author' => 'Alex',
-            ],
-            [
-                'title' => 'Some U.S title 4',
-                'content' => 'Some U.S content 4',
-                'date' => 'January 4, 2021',
-                'author' => 'Manivald',
-            ],
-        ];
-        view('us', compact('title', 'posts'));
+        view('us', compact('title', 'articles'));
     }
 
     public function forms()
